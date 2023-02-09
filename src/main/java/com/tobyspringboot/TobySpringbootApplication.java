4e538eb4c97a1c47a69a62fef99f583954a39a0a
김형익 <kiomnd2@naver.com>
2023-02-09 20:30:15 +0900

    서블릿 요청 처리

--- src/main/java/com/tobyspringboot/TobySpringbootApplication.java
+++ src/main/java/com/tobyspringboot/TobySpringbootApplication.java
@@ -3,16 +3,37 @@ package com.tobyspringboot;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @SpringBootApplication
 public class TobySpringbootApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(TobySpringbootApplication.class, args);
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    String name = req.getParameter("name");
+
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello Servlet "+ name);
+                }
+            }).addMapping("/hello");
+        });
+
         webServer.start();
     }
 
