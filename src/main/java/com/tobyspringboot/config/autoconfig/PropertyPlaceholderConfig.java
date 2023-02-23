d75de5a75d5bc095d21fe5f78adf8af52c6527ba
김형익 <kiomnd2@naver.com>
2023-02-23 23:29:16 +0900

    외부설정을 이용한 자동구성 2

---
+++ src/main/java/com/tobyspringboot/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,14 @@
+package com.tobyspringboot.config.autoconfig;
+
+import com.tobyspringboot.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
