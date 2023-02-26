f8edfad5c33f706602727ade88370d70b7166ef8
kiomnd2 <kiomnd2@naver.com>
2023-02-26 16:43:21 +0900

    jdbc 와 transactionmanerger 구성

---
+++ src/test/java/com/tobyspringboot/DataSourceTest.java
@@ -0,0 +1,22 @@
+package com.tobyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@HellobootTest
+public class DataSourceTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    void connect() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        connection.close();
+    }
+
+}
---
+++ src/test/java/com/tobyspringboot/JdbcTemplateTest.java
@@ -0,0 +1,29 @@
+package com.tobyspringboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@HellobootTest
+public class JdbcTemplateTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init() {
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS HELLO(nmae varchar(50) primary key, count int)");
+
+    }
+
+    @Test
+    void insertAndQuery() {
+        jdbcTemplate.update("insert into hello values(?,?) ", "kim", 3);
+        jdbcTemplate.update("insert into hello values(?,?) ", "Spring", 3);
+
+        Long count = jdbcTemplate.queryForObject("select count(*) from HELLO", Long.class);
+        Assertions.assertThat(count).isEqualTo(2);
+    }
+}
